462d3c349248f3128e5f60c2c0bac8dfd0f2590d
Mh Mitas <mahfuzulmitas@gmail.com>
2024-03-28 17:16:42 +0600

    last commit

--- src/components/loding/Loding.jsx
+++ src/components/loding/Loding.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Audio } from 'react-loader-spinner'
 
 const Loding = () => {
     return (
         <div className='flex justify-center min-h-96 items-center'>
-            <span className="loading loading-spinner loading-lg text-secondary"></span>
+            {/* <span className="loading loading-spinner loading-lg text-secondary"></span> */}
+            <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="violet"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+            />
+
         </div>
     );
 };
